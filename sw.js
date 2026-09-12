0fd5c4458642bf41ed7d7022c9cb0a6c22f9785e
purixawater <arvindmbbokhani8741@gmail.com>
2026-09-12 18:46:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='purixa-crm-pwa-v22.10';
-const APP_SHELL=['./','./index.html','./manifest.json','./purixa-logo.png','./icon-192.png','./icon-512.png','./authorized-signature-clean.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+const CACHE='purixa-crm-v22.11';
+const ASSETS=['./','./index.html','./manifest.json','./purixa-logo.png','./authorized-signature-clean.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
